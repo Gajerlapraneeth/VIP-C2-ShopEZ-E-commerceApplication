199bda14420ac85f30db0249760fcc050d87fb6e
Gajerla Praneeth <buntypraneeth012@gmail.com>
2026-06-17 00:58:46 +0530

    updated backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api"
+  baseURL: "https://vip-c2-shopez-e-commerceapplication-1.onrender.com/api"
 });
 
 export default API;
